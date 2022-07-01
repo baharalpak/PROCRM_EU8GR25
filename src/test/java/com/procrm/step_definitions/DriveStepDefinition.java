261e90bd109c88745c3a4f1b765900717cf9e775
yavuz-55 <yavuzyardim@gmail.com>
2022-07-01 17:15:05 +0100

    Drive scenarios are added

---
+++ src/test/java/com/procrm/step_definitions/DriveStepDefinition.java
@@ -0,0 +1,43 @@
+package com.procrm.step_definitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class DriveStepDefinition {
+
+    @Given("user is on Home Page.")
+    public void user_is_on_home_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("user click on Drive module.")
+    public void user_click_on_drive_module() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("user should be able to see My Driver")
+    public void user_should_be_able_to_see_my_driver() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Given("user is on Driver module.")
+    public void user_is_on_driver_module() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("user click on All Documents.")
+    public void user_click_on_all_documents() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("user should be able to see All Documents.")
+    public void user_should_be_able_to_see_all_documents() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+
+}
