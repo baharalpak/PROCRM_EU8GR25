064b9e3ac10bc8e21505e17dd0bdc9a6f2e7b52c
avtziselma <98188272+avtziselma@users.noreply.github.com>
2022-07-02 17:47:13 +0100

    My Profile feature completed

--- src/test/java/com/procrm/step_definitions/MyProfileStepDefinitions.java
+++ src/test/java/com/procrm/step_definitions/MyProfileStepDefinitions.java
@@ -1,4 +1,14 @@
 package com.procrm.step_definitions;
 
+import com.procrm.pages.MyProfilePage;
+import com.procrm.utilities.Driver;
+import org.openqa.selenium.interactions.Actions;
+
 public class MyProfileStepDefinitions {
+
+    MyProfilePage myProfilePage=new MyProfilePage();
+
+    Actions actions=new Actions(Driver.getDriver());
+
+
 }
