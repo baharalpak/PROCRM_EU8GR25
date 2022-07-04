bd27e763403cfb14bfec71bd682d94e811c288ae
baharalpak <95653223+baharalpak@users.noreply.github.com>
2022-07-04 16:22:59 +0000

    Tasks Step Definitions added.

---
+++ src/test/java/com/procrm/step_definitions/Tasks_StepDefinitions.java
@@ -0,0 +1,69 @@
+package com.procrm.step_definitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class Tasks_StepDefinitions {
+
+    @Given("{string} user is on {string} Page.")
+    public void userIsOnPage(String arg0, String arg1) {
+    }
+
+    @When("{string} user clicks on Tasks module.")
+    public void userClicksOnTasksModule(String arg0) {
+    }
+
+    @Then("{string} user should be able to display {string}.")
+    public void userShouldBeAbleToDisplay(String arg0, String arg1) {
+    }
+
+    @Then("{string} user should be able to see {string}.")
+    public void userShouldBeAbleToSee(String arg0, String arg1) {
+    }
+
+    @When("{string} user clicks {string} button.")
+    public void userClicksButton(String arg0, String arg1) {
+    }
+
+    @And("{string} user fills in {string} which is mandatory field with {string}.")
+    public void userFillsInWhichIsMandatoryFieldWith(String arg0, String arg1, String arg2) {
+    }
+
+    @And("{string} user clicks {string} button to create a new task.")
+    public void userClicksButtonToCreateANewTask(String arg0, String arg1) {
+    }
+
+    @Then("{string} user should be able to display new task created.")
+    public void userShouldBeAbleToDisplayNewTaskCreated(String arg0) {
+    }
+
+    @Then("{string} user CAN NOT BE ABLE TO display new task created.")
+    public void userCANNOTBEABLETODisplayNewTaskCreated(String arg0) {
+    }
+
+    @When("{string} user clicks on the last created task .")
+    public void userClicksOnTheLastCreatedTask(String arg0) {
+    }
+
+    @And("{string} user edits the Task Name as {string} and clicks {string}.")
+    public void userEditsTheTaskNameAsAndClicks(String arg0, String arg1, String arg2) {
+    }
+
+    @And("{string} user clicks {string} button to be able to see {string} Page.")
+    public void userClicksButtonToBeAbleToSeePage(String arg0, String arg1, String arg2) {
+    }
+
+    @Then("{string} user should be able to see the saved changes.")
+    public void userShouldBeAbleToSeeTheSavedChanges(String arg0) {
+    }
+
+    @Then("{string} user CAN NOT BE ABLE TO edit task.")
+    public void userCANNOTBEABLETOEditTask(String arg0) {
+    }
+
+    @Then("{string} user clicks {string} button on {string} Page.")
+    public void userClicksButtonOnPage(String arg0, String arg1, String arg2) {
+    }
+}
