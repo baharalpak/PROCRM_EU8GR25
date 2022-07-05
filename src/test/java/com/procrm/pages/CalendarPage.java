4d1f10cecae56619a2498f693a7032cae5c39642
suleyman aydin <suleymanscience38@gmail.com>
2022-07-05 15:06:27 +0100

    calendar feature added 4

--- src/test/java/com/procrm/pages/CalendarPage.java
+++ src/test/java/com/procrm/pages/CalendarPage.java
@@ -2,6 +2,8 @@ package com.procrm.pages;
 
 import com.procrm.utilities.Driver;
 import io.cucumber.java.ca.Cal;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class CalendarPage extends LoginPage {
@@ -10,10 +12,27 @@ public class CalendarPage extends LoginPage {
         PageFactory.initElements(Driver.getDriver(),this);
     }
 
-    @Override
-    public void login(String username, String password) {
-        super.login(username, password);
-    }
+    @FindBy(xpath = "(//span[@class='menu-item-link-text'])[6]")
+    public WebElement calendarButton;
+
+    @FindBy(className = "ui-btn-main")
+    public WebElement addButton;
+
+    @FindBy(id = "calendar_slider_15039_entry_name")
+    public WebElement eventNameBox;
+
+    @FindBy(id = "calendar_slider_15039_date_from")
+    public WebElement dateBox;
+
+    @FindBy(id = "calendar_slider_15039_date_full_day")
+    public WebElement allDayBox;
+
+    @FindBy(id = "calendar_slider_15039_save_cmd")
+    public WebElement saveButton;
+
+
+
+
 
 
 }
--- src/test/java/com/procrm/step_definitions/Calendar_StepDefinitions.java
+++ src/test/java/com/procrm/step_definitions/Calendar_StepDefinitions.java
@@ -1,9 +1,12 @@
 package com.procrm.step_definitions;
 
+import com.procrm.pages.BasePage;
 import com.procrm.pages.CalendarPage;
 import com.procrm.pages.LoginPage;
+import com.procrm.utilities.BrowserUtilities;
 import com.procrm.utilities.ConfigurationReader;
 import com.procrm.utilities.Driver;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -14,73 +17,72 @@ import static org.openqa.selenium.Keys.ENTER;
 
 public class Calendar_StepDefinitions {
 
-    CalendarPage calendarPage = new CalendarPage();
-    LoginPage loginPage = new LoginPage();
 
+    CalendarPage calendarPage = new CalendarPage();
 
     @Given("User is on Agileprocrm url successfully")
     public void user_is_on_agileprocrm_url_successfully() {
-        Driver.getDriver().get(ConfigurationReader.getProperty("url"));
-    }
-
-
-    @When("User enters {string} and {string}")
-    public void user_enters_and(String username, String password) {
-        calendarPage.login(username,password);
+       Driver.getDriver().get(ConfigurationReader.getProperty("url"));
 
     }
 
+    @Given("HR User is on homepage")
+    public void hr_user_is_on_homepage() {
 
-    @When("User click on Log In button")
-    public void user_click_on_button() {
-        loginPage.loginButton.click();
+        BasePage.loginAsHR();
 
     }
 
 
+    @When("User click Calendar button")
+    public void user_click_calendar_button() {
 
-    @When("User should land on home page")
-    public void user_should_land_on_home_page() {
 
+        calendarPage.calendarButton.click();
+
+    }
+    @Then("User should be able to display My Calendar page")
+    public void user_should_be_able_to_display_my_calendar_page() {
+
+       Assert.assertTrue(Driver.getDriver().getTitle().contains("Calendar"));
 
     }
 
 
-
-    @When("User click {string} button")
-    public void user_click_button(String calendar) {
-
-
-    }
-    @Then("User verify displaying {string} function successfully")
-    public void user_verify_displaying_function_successfully(String string) {
-
-    }
-
-    @When("User enters username {string} and  password {string}")
-    public void user_enters_username_and_password(String string, String string2) {
-
-    }
-
-    @When("User display {string} function")
-    public void user_display_function(String string) {
-
-    }
-    @When("User should be able to see {string} page")
-    public void user_should_be_able_to_see_page(String string) {
-
-    }
-    @Then("Verify that User {string} the {string} on My Calendar page successfully")
-    public void verify_that_user_the_on_my_calendar_page_successfully(String string, String string2) {
-
+    @Given("Marketing User is on homepage")
+    public void marketingUserIsOnHomepage() {
+        BasePage.loginAsMarketing();
     }
 
 
+    @Given("Helpdesk User is on homepage")
+    public void helpdeskUserIsOnHomepage() {
+        BasePage.loginAsHelpDesk();
+    }
+
+    @When("User click ADD button")
+    public void userClickADDButton() {
+        calendarPage.addButton.click();
+        BrowserUtilities.sleep(5);
+    }
+
+    @And("Fill event name as {string} and date as {string} and click All day checkbox and save the event")
+    public void fillEventNameAsAndDateAsAndClickAllDayCheckboxAndSaveTheEvent(String eventName, String eventDate) {
+
+        //calendarPage.eventNameBox.click();
+        //calendarPage.eventNameBox.clear();
+        BrowserUtilities.sleep(5);
+        calendarPage.eventNameBox.sendKeys(eventName);
+        calendarPage.dateBox.sendKeys(eventDate);
+        calendarPage.allDayBox.click();
+        BrowserUtilities.sleep(5);
+        calendarPage.saveButton.click();
+    }
+
+    @Then("User should be able to see event is created on the calendar")
+    public void userShouldBeAbleToSeeEventIsCreatedOnTheCalendar() {
 
 
-
-
-
-
+    }
 
 }
