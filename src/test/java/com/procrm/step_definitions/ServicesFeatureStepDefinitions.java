ea94fd3e7acd581e7a66a3b4fe1f1de7c07a980e
Mustafa Yurdakul <mustafaa.yurdakul@gmail.com>
2022-07-06 20:51:28 +0100

    Services Feature is completed

--- src/test/java/com/procrm/step_definitions/ServicesFeatureStepDefinitions.java
+++ src/test/java/com/procrm/step_definitions/ServicesFeatureStepDefinitions.java
@@ -8,6 +8,7 @@ import io.cucumber.java.en.When;
 import com.procrm.pages.BasePage;
 import org.junit.Assert;
 
+import java.util.HashMap;
 import java.util.Locale;
 
 public class ServicesFeatureStepDefinitions {
@@ -32,8 +33,8 @@ public class ServicesFeatureStepDefinitions {
         servicesPage.trainingTab.click();
         BrowserUtilities.sleep(2);
     }
-    @When("User clicks on the my courses tab to see his\\/her courses")
-    public void user_clicks_on_the_my_courses_tab_to_see_his_her_courses() {
+    @When("User clicks on the my courses tab to see their courses")
+    public void user_clicks_on_the_my_courses_tab_to_see_their_courses() {
         servicesPage.myCourses.click();
         BrowserUtilities.sleep(2);
     }
@@ -41,18 +42,53 @@ public class ServicesFeatureStepDefinitions {
     public void should_be_displayed(String str) {
         switch (str) {
             case "My Courses":
-                Assert.assertTrue(servicesPage.isCourseReportTitleDisplayed());
+                Assert.assertEquals("Course report",servicesPage.courseReportPageTitle());
                 Assert.assertTrue(servicesPage.isCoursesTableDisplayed());
                 break;
             case "Grade Book":
-                //
+                Assert.assertEquals("Test results",servicesPage.testResultsPageTitle());
+                Assert.assertTrue(servicesPage.isGradeBookTableDisplayed());
                 break;
             default:
                 //
         }
     }
+    @When("User clicks on the grade book tab to see their grades")
+    public void user_clicks_on_the_grade_book_tab_to_see_their_grades() {
+        servicesPage.gradeBook.click();
+        BrowserUtilities.sleep(2);
+    }
 
+    @When("User clicks on the profile tab.")
+    public void user_clicks_on_the_profile_tab() {
+        servicesPage.profile.click();
+        BrowserUtilities.sleep(2);
+    }
+    @When("User edits the student profile and clicks on the save button")
+    public void user_edits_the_student_profile_and_clicks_on_the_save_button() {
+        servicesPage.editPersonalData();
+        servicesPage.editProfileSetting();
+        servicesPage.editPostalAddress();
+        servicesPage.saveProfileChanges();
 
+    }
+    @Then("Student's Profile data should be edited successfully")
+    public void student_s_profile_data_should_be_edited_successfully() {
+        HashMap hm1 = new HashMap();
+        hm1 = (HashMap) servicesPage.retrievePersonalData();
+        Assert.assertEquals("Mustafa",hm1.get("FirstName"));
+        Assert.assertEquals("Y",hm1.get("LastName"));
+        Assert.assertEquals("my@gmail.com",hm1.get("Email"));
+        Assert.assertEquals("www.xxxxx.com",hm1.get("Website"));
+        Assert.assertEquals("123",hm1.get("ICQ"));
+
+        HashMap hm2 = new HashMap();
+        hm2 = (HashMap) servicesPage.retrievePostalAddress();
+        Assert.assertEquals("United Kingdom",hm2.get("Country"));
+        Assert.assertEquals("AA12 3BC",hm2.get("ZipCode"));
+
+        Assert.assertTrue(servicesPage.chkPublicProfile.isSelected());
+    }
 
 
 }
