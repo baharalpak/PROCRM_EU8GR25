1664d898642088869c0359794479972eaa1359b8
cemaleddinbozkus <98359310+cemaleddinbozkus@users.noreply.github.com>
2022-07-24 00:57:15 +0200

    CukesRunner changed

--- src/test/java/com/procrm/runners/CukesRunner.java
+++ src/test/java/com/procrm/runners/CukesRunner.java
@@ -15,6 +15,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/procrm/step_definitions",
         dryRun = false,
+
         tags = "@Scrum-893"
 
 
