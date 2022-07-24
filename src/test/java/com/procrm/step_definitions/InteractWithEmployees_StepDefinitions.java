d3f62acea8de1dccf2c3ad1a0fa8f304394e823e
suleyman aydin <suleymanscience38@gmail.com>
2022-07-24 09:46:05 +0100

    scrum-896 updated

--- src/test/java/com/procrm/step_definitions/InteractWithEmployees_StepDefinitions.java
+++ src/test/java/com/procrm/step_definitions/InteractWithEmployees_StepDefinitions.java
@@ -1,4 +1,148 @@
 package com.procrm.step_definitions;
 
+import com.procrm.pages.BasePage;
+import com.procrm.pages.InteractWithEmployeesPage;
+import com.procrm.utilities.BrowserUtilities;
+import com.procrm.utilities.Driver;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.interactions.Actions;
+
+import javax.swing.*;
+import java.text.DateFormat;
+import java.util.Date;
+
 public class InteractWithEmployees_StepDefinitions {
+    InteractWithEmployeesPage interactWithEmployeesPage= new InteractWithEmployeesPage();
+    Date date = new Date();
+
+
+    @Given("{string} user is on homepage.")
+    public void user_is_on_homepage(String userType) {
+
+        switch (userType.toLowerCase()) {
+            case "hr":
+                BasePage.loginAsHR();
+                break;
+            case "helpdesk":
+                BasePage.loginAsHelpDesk();
+                break;
+            case "marketing":
+                BasePage.loginAsMarketing();
+                break;
+        }
+
+        BrowserUtilities.sleep(5);
+    }
+
+
+    @When("User click comment button other employees' posts")
+    public void user_click_comment_button_other_employees_posts() {
+
+        interactWithEmployeesPage.commentButton.click();
+        BrowserUtilities.sleep(5);
+    }
+
+    @When("User write comment in the INPUT BOX {string}")
+    public void user_write_comment_in_the_input_box(String message) {
+        Driver.getDriver().switchTo().frame(interactWithEmployeesPage.commentBoxIFrame); //iFrame switch
+
+        BrowserUtilities.waitForClickablility(interactWithEmployeesPage.commentInputBox,5);
+        interactWithEmployeesPage.commentInputBox.click();
+        interactWithEmployeesPage.commentInputBox.sendKeys(String.valueOf(date));  //Will be fixed
+    }
+
+    @When("User click the send button at the INPUT BOX.")
+    public void user_click_the_send_button_at_the_input_box() {
+        Driver.getDriver().switchTo().defaultContent();
+
+        interactWithEmployeesPage.sendButton.click();
+    }
+
+    @Then("User should see own comment on other employees' posts is applicable.")
+    public void user_should_see_own_comment_on_other_employees_posts_is_applicable() {
+        Assert.assertTrue(Driver.getDriver().findElement(By.xpath("(//div[contains(text(),"+ date + ")]")).isDisplayed()); //Will be fixed
+    }
+
+
+    @When("User click the like button at the INPUT BOX.")
+    public void userClickTheLikeButtonAtTheINPUTBOX() {
+
+        interactWithEmployeesPage.likeButton.click();
+        BrowserUtilities.sleep(5);
+    }
+
+    @Then("User should see like on other employees' posts.")
+    public void userShouldSeeLikeOnOtherEmployeesPosts() {
+
+        Assert.assertTrue(interactWithEmployeesPage.likeButtonIsVisible.isDisplayed());
+    }
+
+    @When("User click the unfollow button at the INPUT BOX.")
+    public void userClickTheUnfollowButtonAtTheINPUTBOX() {
+
+        interactWithEmployeesPage.unfollowButton.click();
+        BrowserUtilities.sleep(5);
+
+
+    }
+
+    @Then("User should see follow button on other employees' posts.")
+    public void userShouldSeeFollowButtonOnOtherEmployeesPosts() {
+
+        Assert.assertTrue(interactWithEmployeesPage.followButtonIsVisible.isDisplayed());
+
+
+    }
+
+    @When("User click the like button at the reviewers's comments.")
+    public void userClickTheLikeButtonAtTheReviewersSComments() {
+        interactWithEmployeesPage.likeButtonReviwers.click();
+        BrowserUtilities.sleep(5);
+    }
+
+    @Then("User should see like on other reviewers's comments.")
+    public void userShouldSeeLikeOnOtherReviewersSComments() {
+        Assert.assertTrue(interactWithEmployeesPage.getLikeButtonReviwersIsVisible.isDisplayed());
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+    @When("User click the reviewers' name")
+    public void userClickTheReviewersName() {
+
+        interactWithEmployeesPage.clickReviwersName.click();
+        BrowserUtilities.sleep(5);
+
+    }
+
+    @Then("User should be able to see reviewers' profiles.")
+    public void userShouldBeAbleToSeeReviewersProfiles() {
+        Assert.assertTrue(interactWithEmployeesPage.displayReviwersProfiles.isDisplayed());
+
+    }
+
+    @When("User click the star icon.")
+    public void userClickTheStarIcon() {
+        interactWithEmployeesPage.starIconButton.click();
+        BrowserUtilities.sleep(5);
+    }
+
+    @Then("User should be able to see yellow star icon is applicable")
+    public void userShouldBeAbleToSeeYellowStarIconIsApplicable() {
+        Assert.assertTrue(interactWithEmployeesPage.seeYellowStarIcon.isDisplayed());
+    }
 }
--- src/test/resources/features/ InteractWithEmployees.feature
+++ src/test/resources/features/ InteractWithEmployees.feature
@@ -7,13 +7,82 @@ Feature: Interact With Employees on the Posts
 
   Background:
     Given User is on Agileprocrm url successfully
-@SCRUM-896
-  Scenario Outline: User can make a comment on other employees' posts.
+
+  @SCRUM-896
+  Scenario Outline: Verify that User can make a comment on other employees' posts.
     Given "<user>" user is on homepage.
     When User click comment button other employees' posts
     And User write comment in the INPUT BOX "Thank you"
     And User click the send button at the INPUT BOX.
     Then User should see own comment on other employees' posts is applicable.
+
+    Examples:
+      | user      |
+      | hr        |
+      | marketing |
+      | helpdesk  |
+
+  Scenario Outline: Verify that User can make a like on other employees' posts.
+    Given "<user>" user is on homepage.
+    When User click the like button at the INPUT BOX.
+    Then User should see like on other employees' posts.
+
+    Examples:
+      | user      |
+      | hr        |
+      | marketing |
+      | helpdesk  |
+
+  Scenario Outline: Verify that User can make a unfollow on other employees' posts.
+    Given "<user>" user is on homepage.
+    When User click the unfollow button at the INPUT BOX.
+    Then User should see follow button on other employees' posts.
+
+    Examples:
+      | user      |
+      | hr        |
+      | marketing |
+      | helpdesk  |
+
+  Scenario Outline: Verify that User should be able to like on all other reviewers's comments.
+    Given "<user>" user is on homepage.
+    When User click the like button at the reviewers's comments.
+    Then User should see like on other reviewers's comments.
+
+    Examples:
+      | user      |
+      | hr        |
+      | marketing |
+      | helpdesk  |
+
+  Scenario Outline: Verify that User should be able to comments on all other reviewers's comments.
+    Given "<user>" user is on homepage.
+    When User click the comments button at the reviewers's comments.
+    And User write comment in the INPUT BOX "Thank you" at the reviewers's comments.
+    And User click the send button at the INPUT BOX at the reviewers's comments.
+    Then User should see own comment on other reviewers's comments is applicable.
+
+    Examples:
+      | user      | interaction |
+      | hr        | cry         |
+      | marketing | kiss        |
+      | helpdesk  | like        |
+
+  Scenario Outline: Verify that User should be able to click on reviewers' name and visit their profiles.
+    Given "<user>" user is on homepage.
+    When User click the reviewers' name
+    Then User should be able to see reviewers' profiles.
+
+    Examples:
+      | user      |
+      | hr        |
+      | marketing |
+      | helpdesk  |
+
+  Scenario Outline: Verify that User should be able to add others' posts to favorite by clicking on the Star icon.
+    Given "<user>" user is on homepage.
+    When User click the star icon.
+    Then User should be able to see yellow star icon is applicable
 
     Examples:
       | user      |
