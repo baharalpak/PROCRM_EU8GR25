fdddca89b4193494378275f1dd531dd601285aef
f3ktr <bahriyelifek@gmail.com>
2022-07-25 21:24:37 +0200

    Deleted unnecessary spaces in code file

--- src/test/java/com/procrm/step_definitions/InteractWithEmployees_StepDefinitions.java
+++ src/test/java/com/procrm/step_definitions/InteractWithEmployees_StepDefinitions.java
@@ -18,13 +18,12 @@ import java.text.DateFormat;
 import java.util.Date;
 
 public class InteractWithEmployees_StepDefinitions {
-    InteractWithEmployeesPage interactWithEmployeesPage= new InteractWithEmployeesPage();
+    InteractWithEmployeesPage interactWithEmployeesPage = new InteractWithEmployeesPage();
     Date date = new Date();
 
 
     @Given("{string} user is on homepage.")
     public void user_is_on_homepage(String userType) {
-
 
         switch (userType.toLowerCase()) {
             case "hr":
@@ -53,7 +52,7 @@ public class InteractWithEmployees_StepDefinitions {
     public void user_write_comment_in_the_input_box(String message) {
         Driver.getDriver().switchTo().frame(interactWithEmployeesPage.commentBoxIFrame); //iFrame switch
 
-        BrowserUtilities.waitForClickablility(interactWithEmployeesPage.commentInputBox,5);
+        BrowserUtilities.waitForClickablility(interactWithEmployeesPage.commentInputBox, 5);
         interactWithEmployeesPage.commentInputBox.click();
         interactWithEmployeesPage.commentInputBox.sendKeys(String.valueOf(date));
     }
@@ -70,72 +69,65 @@ public class InteractWithEmployees_StepDefinitions {
         Assert.assertTrue(interactWithEmployeesPage.commentIsVisible.isDisplayed());
     }
 
-        @When("User click the like button at the INPUT BOX.")
-        public void userClickTheLikeButtonAtTheINPUTBOX () {
+    @When("User click the like button at the INPUT BOX.")
+    public void userClickTheLikeButtonAtTheINPUTBOX() {
 
-            interactWithEmployeesPage.likeButton.click();
-            BrowserUtilities.sleep(5);
-        }
+        interactWithEmployeesPage.likeButton.click();
+        BrowserUtilities.sleep(5);
+    }
 
-        @Then("User should see like on other employees' posts.")
-        public void userShouldSeeLikeOnOtherEmployeesPosts () {
+    @Then("User should see like on other employees' posts.")
+    public void userShouldSeeLikeOnOtherEmployeesPosts() {
+        Assert.assertTrue(interactWithEmployeesPage.likeButtonIsVisible.isDisplayed());
+    }
 
-            Assert.assertTrue(interactWithEmployeesPage.likeButtonIsVisible.isDisplayed());
-        }
+    @When("User click the unfollow button at the INPUT BOX.")
+    public void userClickTheUnfollowButtonAtTheINPUTBOX() {
+        interactWithEmployeesPage.unfollowButton.click();
+        BrowserUtilities.sleep(5);
+    }
 
-        @When("User click the unfollow button at the INPUT BOX.")
-        public void userClickTheUnfollowButtonAtTheINPUTBOX () {
+    @Then("User should see follow button on other employees' posts.")
+    public void userShouldSeeFollowButtonOnOtherEmployeesPosts() {
+        Assert.assertTrue(interactWithEmployeesPage.followButtonIsVisible.isDisplayed());
+    }
 
-            interactWithEmployeesPage.unfollowButton.click();
-            BrowserUtilities.sleep(5);
+    @When("User click the like button at the reviewers's comments.")
+    public void userClickTheLikeButtonAtTheReviewersSComments() {
+        interactWithEmployeesPage.likeButtonReviwers.click();
+        BrowserUtilities.sleep(5);
+    }
 
-
-        }
-
-        @Then("User should see follow button on other employees' posts.")
-        public void userShouldSeeFollowButtonOnOtherEmployeesPosts () {
-
-            Assert.assertTrue(interactWithEmployeesPage.followButtonIsVisible.isDisplayed());
-
-
-        }
-
-        @When("User click the like button at the reviewers's comments.")
-        public void userClickTheLikeButtonAtTheReviewersSComments () {
-            interactWithEmployeesPage.likeButtonReviwers.click();
-            BrowserUtilities.sleep(5);
-        }
-
-        @Then("User should see like on other reviewers's comments.")
-        public void userShouldSeeLikeOnOtherReviewersSComments () {
-            Assert.assertTrue(interactWithEmployeesPage.getLikeButtonReviwersIsVisible.isDisplayed());
-        }
+    @Then("User should see like on other reviewers's comments.")
+    public void userShouldSeeLikeOnOtherReviewersSComments() {
+        Assert.assertTrue(interactWithEmployeesPage.getLikeButtonReviwersIsVisible.isDisplayed());
+    }
 
 
-        @When("User click the reviewers' name")
-        public void userClickTheReviewersName () {
+    @When("User click the reviewers' name")
+    public void userClickTheReviewersName() {
 
-            interactWithEmployeesPage.clickReviwersName.click();
-            BrowserUtilities.sleep(5);
+        interactWithEmployeesPage.clickReviwersName.click();
+        BrowserUtilities.sleep(5);
 
-        }
+    }
 
-        @Then("User should be able to see reviewers' profiles.")
-        public void userShouldBeAbleToSeeReviewersProfiles () {
-            Assert.assertTrue(interactWithEmployeesPage.displayReviwersProfiles.isDisplayed());
+    @Then("User should be able to see reviewers' profiles.")
+    public void userShouldBeAbleToSeeReviewersProfiles() {
+        Assert.assertTrue(interactWithEmployeesPage.displayReviwersProfiles.isDisplayed());
 
-        }
+    }
 
-        @When("User click the star icon.")
-        public void userClickTheStarIcon () {
-            interactWithEmployeesPage.starIconButton.click();
-            BrowserUtilities.sleep(5);
-        }
+    @When("User click the star icon.")
+    public void userClickTheStarIcon() {
+        interactWithEmployeesPage.starIconButton.click();
+        BrowserUtilities.sleep(5);
+    }
 
-        @Then("User should be able to see yellow star icon is applicable")
-        public void userShouldBeAbleToSeeYellowStarIconIsApplicable () {
-            Assert.assertTrue(interactWithEmployeesPage.seeYellowStarIcon.isDisplayed());
-        }
+    @Then("User should be able to see yellow star icon is applicable")
+    public void userShouldBeAbleToSeeYellowStarIconIsApplicable() {
+        Assert.assertTrue(interactWithEmployeesPage.seeYellowStarIcon.isDisplayed());
+    }
 
     @When("User click the {string} button at the employees' posts.")
     public void userClickTheButtonAtTheReviewersSComments(String interactionButton) {
@@ -160,15 +152,13 @@ public class InteractWithEmployees_StepDefinitions {
     }
 
 
-
-
     @Then("User should see {string} on other employees' posts.")
     public void userShouldSeeOnOtherReviewersSComments(String interactionButton) {
 
 
         switch (interactionButton.toLowerCase()) {
             case "cry":
-               Assert.assertTrue(interactWithEmployeesPage.cryButtonIsVisible.isDisplayed());
+                Assert.assertTrue(interactWithEmployeesPage.cryButtonIsVisible.isDisplayed());
                 break;
             case "kiss":
                 Assert.assertTrue(interactWithEmployeesPage.kissButtonISVisible.isDisplayed());
@@ -177,52 +167,37 @@ public class InteractWithEmployees_StepDefinitions {
                 Assert.assertTrue(interactWithEmployeesPage.laughButtonIsVisible.isDisplayed());
                 break;
         }
-
-
-
     }
 
     @When("User click the comments button at the reviewers's comments.")
     public void userClickTheCommentsButtonAtTheReviewersSComments() {
-
         interactWithEmployeesPage.reviwersReplyButton.click();
-
     }
 
     @And("User write comment in the INPUT BOX at the reviewers's comments.")
     public void userWriteCommentInTheINPUTBOXAtTheReviewersSComments() {
-
         Driver.getDriver().switchTo().frame(interactWithEmployeesPage.commentBoxIFrame); //iFrame switch
 
-        BrowserUtilities.waitForClickablility(interactWithEmployeesPage.commentInputBox,5);
+        BrowserUtilities.waitForClickablility(interactWithEmployeesPage.commentInputBox, 5);
 
         interactWithEmployeesPage.commentInputBox.click();
         interactWithEmployeesPage.commentInputBox.sendKeys(String.valueOf(date));
-
-
-
     }
 
     @And("User click the send button at the INPUT BOX at the reviewers's comments.")
     public void userClickTheSendButtonAtTheINPUTBOXAtTheReviewersSComments() {
-
         Driver.getDriver().switchTo().defaultContent();
         BrowserUtilities.sleep(3);
         interactWithEmployeesPage.sendButton.click();
-
     }
 
     @Then("User should see own comment on other reviewers's comments is applicable.")
     public void userShouldSeeOwnCommentOnOtherReviewersSCommentsIsApplicable() {
-
         Assert.assertTrue(interactWithEmployeesPage.commentIsVisible.isDisplayed());
-
-
     }
 
     @Then("User should see error message {string} in INPUT BOX.")
     public void userShouldSeeErrorMessageInINPUTBOX(String message) {
-
         Assert.assertTrue(interactWithEmployeesPage.errorMessage.getText().contains(message));
     }
 }
